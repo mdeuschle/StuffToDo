1f0e649953efff022a38ac27611075c37c832b01
Matt Deuschle <mdeuschle@gmail.com>
2018-04-11 18:29:03 -0700

    add initial UI

---
+++ StuffToDo/Controller/RootVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  StuffToDo
+//
+//  Created by Matt Deuschle on 4/11/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
