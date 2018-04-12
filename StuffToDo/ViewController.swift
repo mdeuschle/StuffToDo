4c153c486c80a2a2c367d2a09c0b8b0c4cfcf87c
Matt Deuschle <mdeuschle@gmail.com>
2018-04-11 18:07:36 -0700

    Initial Commit

---
+++ StuffToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StuffToDo
+//
+//  Created by Matt Deuschle on 4/11/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
